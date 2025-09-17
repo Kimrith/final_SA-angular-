5649c7bc0c27326d781cb4af55a784a6259241f2
Chey Kimirth <cheykimrith123@gmail>
2025-09-17 23:09:11 +0700

    V5

--- src/Components/cetecory/cetecory.ts
+++ src/Components/cetecory/cetecory.ts
@@ -9,9 +9,9 @@ import { Component } from '@angular/core';
 })
 export class Cetecory {
   products = [
-    { id: 1, name: 'Soft Drink' },
-    { id: 2, name: 'Soft Drink' },
-    { id: 3, name: 'Soft Drink' },
-    { id: 4, name: 'Soft Drink' },
+    { id: 1, name: 'Food' },
+    { id: 1, name: 'Solf Drink' },
+    { id: 1, name: 'Fruit' },
+    { id: 8, name: 'Other' },
   ];
 }
